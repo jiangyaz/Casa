d972649a14a7d9b1710997aeeedebdec2f072bb6
jiangyaz <jiangyaz@usc.edu>
2014-01-24 23:41:57 -0800

    adding initial files

---
+++ Casa/CasaAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CasaAppDelegate.h
+//  Casa
+//
+//  Created by Jiangyang Zhang on 1/24/14.
+//  Copyright (c) 2014 HackTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CasaAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Casa/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Casa
+//
+//  Created by Jiangyang Zhang on 1/24/14.
+//  Copyright (c) 2014 HackTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CasaAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CasaAppDelegate class]));
+    }
+}
